bbe3b7771a046d3ae00c488c796f1efe7dcba4c8
David <david.selleron@neuf.fr>
2018-02-02 20:08:17 +0100

    correction requete change id project

--- configuration/requests/change_cegid_project_id.sql
+++ configuration/requests/change_cegid_project_id.sql
@@ -1,8 +1,8 @@
 -- changer le CEGID d'un projet
--- bascule les pointages d'un projet � un autre
+-- bascule les pointages d'un projet a un autre
 -- avant d'executer positionner :
--- @ID_OLD
--- @ID_NEW  -- doit exister
+-- @ID_OLD  -- sera positionné sur Annule
+-- @ID_NEW  -- sera créé
 -- parfois, il faut reediter le nom du projet pour que les changements soit pris en compte
 
 
@@ -15,8 +15,11 @@ START TRANSACTION;
 set @ID_OLD="P18003";
 set @ID_NEW="P18015";
 
--- DELETE FROM cegid_project WHERE CEGID=@ID_NEW;
-UPDATE cegid_project                 	   SET CEGID=@ID_NEW WHERE CEGID=@ID_OLD;
+
+INSERT INTO `cegid_project` (`CEGID`, `NAME`, `PRIX_VENTE`, `DEBUT`, `FIN`, `FIN_GARANTIE`, `STATUS`, `TYPE`, `GROUPE`, `COMMENTAIRE`, `VISIBLE`) 
+SELECT @ID_NEW, concat(`NAME`,"_"), `PRIX_VENTE`, `DEBUT`, `FIN`, `FIN_GARANTIE`, `STATUS`, `TYPE`, `GROUPE`, `COMMENTAIRE`, `VISIBLE` FROM `cegid_project`
+WHERE CEGID=@ID_OLD;
+
 
 UPDATE cegid_devis_project                 SET CEGID=@ID_NEW WHERE CEGID=@ID_OLD;
 UPDATE cegid_pointage                      SET PROJECT_ID=@ID_NEW WHERE PROJECT_ID=@ID_OLD;
@@ -25,10 +28,10 @@ UPDATE cegid_pointage_previsionnel_history SET PROJECT_ID=@ID_NEW WHERE PROJECT_
 UPDATE cegid_pointage_voulu                SET PROJECT_ID=@ID_NEW WHERE PROJECT_ID=@ID_OLD;
 UPDATE cegid_project_cout                  SET PROJECT_ID=@ID_NEW WHERE PROJECT_ID=@ID_OLD;
 
-DELETE FROM cegid_project WHERE CEGID=@ID_OLD;
-
+UPDATE `cegid_project` SET `STATUS` = 'Annule' WHERE `cegid_project`.`CEGID` = @ID_OLD;
+-- DELETE FROM cegid_project WHERE CEGID=@ID_OLD;
 
 
 SET FOREIGN_KEY_CHECKS=1;
--- ROLLBACK;
-COMMIT;
+ROLLBACK;
+-- COMMIT;
